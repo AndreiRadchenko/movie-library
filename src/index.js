2b150eb79eb93b9ca73da36c8c7edd7af7b76b43
AndreiRadchenko <d4100kx@gmail.com>
2022-10-27 16:27:09 +0300

    impotr all js to index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-import './js/modal-login.js';
+import './js/index/header';
+import './js/index/gallery';
+import './js/index/pagination';
+import './js/index/footer';
+
+import './js/library/header';
+
+import './js/modal-login';
+import './js/modal-detail';
+import './js/button-up';
+import './js/modal-spinner';
+import './js/modal-trailer';
