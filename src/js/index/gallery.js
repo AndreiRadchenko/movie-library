264fc36b614035b57313268106a445442b51f966
SvitlanaStadnyk <svitstadnyk@gmail.com>
2022-10-29 18:39:57 +0300

    gallery bagfix
    1. підвантажила шрифти с googlefonts до index.html
    2. доробила під мобiльну, таблеточну, декстопну версії стилi для галереї
    3. почистала зайвий закоментований код у gallery.js

--- src/js/index/gallery.js
+++ src/js/index/gallery.js
@@ -29,9 +29,9 @@ export const Gallery = galleryArray => {
         release_date,
         vote_average,
       }) => `<div class="film__card">
-        <a class="film__link link" href="">
-          <img class="film__img" src="https://image.tmdb.org/t/p/w500/${poster_path}" alt="" loading="lazy" />
           <div class="film__info">
+          <a class="film__link" href="">
+          <img class="film__img" src="https://image.tmdb.org/t/p/w500/${poster_path}" alt="" loading="lazy" />
            <h3 class="film__title">${original_title}</h3>
           <div class="film__details">
           <div class="film__genre">${genre_ids}</div>
@@ -78,84 +78,3 @@ export const renderGallery = galleryArray => {
 };
 
 filmGallery.addEventListener('click', renderModalDetail);
-
-// const refs = getRefs();
-
-// export default function MarkUp(data) {
-//   const markUp = data
-//     .map(
-//       ({
-//         poster_path,
-//         title,
-//         genre_ids,
-//         release_date,
-//         id,
-//         vote_average: rating,
-//       }) => {
-//         const genresToCards = createGenresText(genre_ids);
-//         return `
-//       <div class="film__card">
-//         <a href="javascript:void(0)" class="film__link">
-//             <picture>
-//                 <source srcset="
-//                 ${setPoster(poster_path, 'original')} 1x,
-//                 ${setPoster(poster_path, 'original')} 2x
-//                 "
-//                 media="(min-width: 1024px)"
-//                 >
-//                 <source srcset="
-//                 ${setPoster(poster_path, 'w500')} 1x,
-//                 ${setPoster(poster_path, 'w500')} 2x
-//                 "
-//                 media="(min-width: 768px)"
-//                 >
-//               <source srcset="
-//                 ${setPoster(poster_path, 'w300')} 1x,
-//                 ${setPoster(poster_path, 'w300')} 2x
-//                 "
-//                 >
-//               <img src="${setPoster(poster_path, 'w300')}"
-//               alt=""
-//               class="film__img"
-//               data-id="${id}"
-//               loading="lazy"/>
-//             </picture>
-//           </div>
-//           <div class="film__info">
-//             <h3 class="film__name">${trimStr(title.toUpperCase())}</h3>
-//             <div class="film__details">
-//               <div class="film__genre">${trimStr(genresToCards)}</div>
-//                 <div class="film__year">${setReleaseDate(release_date)}</div>
-//                  <div class="film__rating">${rating}</div>
-//               </div>
-//           </div>
-//           </div>
-//         </a>
-//       </div>`;
-//       }
-//     )
-//     .join('');
-
-//   refs.gallery.insertAdjacentHTML('beforeend', markUp);
-// }
-
-// function setPoster(poster, imgSize) {
-//   if (poster === null) {
-//     return 'https://wipfilms.net/wp-content/data/posters/tt0338683.jpg';
-//   }
-
-//   return `https://image.tmdb.org/t/p/${imgSize}${poster}`;
-// }
-
-// function setReleaseDate(year) {
-//   if (!year) {
-//     return 'No data';
-//   }
-//   return year.slice(0, 4);
-// }
-// function trimStr(str) {
-//   if (str.length > 33) {
-//     return `${str.substring(0, 30)} <span class="string">...</span>`;
-//   }
-//   return str;
-// }
