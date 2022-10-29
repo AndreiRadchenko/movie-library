6514e4dde571017510897a9091a7c77d858eb637
YuriySamusew <6177385@gmail.com>
2022-10-29 21:21:55 +0300

    serch film in moviedb

--- src/js/moviedb/moviedb.js
+++ src/js/moviedb/moviedb.js
@@ -2,70 +2,103 @@
 // Ось мій ключ: api_key=9673c8c8f98cb6e489d5cad6b3789836
 //
 // Потрібно реалізувати наступні методи:
-// 1. getTrending(page=1) --> повертає масив фільмів. Приймає номер сторінки, за замовчуванням 1
-// 2. searchForMovie(movieName, page=1) --> повертає масив фільмів за рядком пошуку
-// 3. getMovieDetail(movieId)
+// 1.+ getFilmsPopular(page=1) --> повертає масив фільмів. Приймає номер сторінки, за замовчуванням 1
+// 2.+ getFilmsById(id)
+
+// 3. searchForMovie(movieName, page=1) --> повертає масив фільмів за рядком пошуку
 // 4. getMovieTrailer(moiveId)
-// 5. getGenres()    https://api.themoviedb.org/3/genre/movie/list?api_key=9673c8c8f98cb6e489d5cad6b3789836&language=en-US
 //
 // Клас має містити властивості totalResults, currentPage які будуть оновлюватися при кожному запиті
 //
-//https://api.themoviedb.org/3/trending/movie/day?api_key=9673c8c8f98cb6e489d5cad6b3789836&page=2
+
 //api.themoviedb.org/3/movie/913290?api_key=9673c8c8f98cb6e489d5cad6b3789836
 
 import axios from 'axios';
-const BASE_URL = 'https://api.themoviedb.org/3/';
-const API_KEY = '9673c8c8f98cb6e489d5cad6b3789836';
 
-export default class FetchImagesService {
+export default class FetchMoviService {
   constructor() {
-    this.searchQuery = '';
+    // this.searchQuery = '';
     this.page = 1;
     this.total = 0;
     this.fetchedImages = 0;
+    this.query = '';
   }
-};
 
-  BASE_URL = 'https://pixabay.com/api/';
+  BASE_URL = 'https://api.themoviedb.org/3/';
+  POPULAR_URL = `${this.BASE_URL}/trending/movie/day`;
+  SEARCH_URL = `${this.BASE_URL}/search/movie`;
+  MOVIE_ID_URL = `${this.BASE_URL}/movie/`;
   RESPONSE_OK = 200;
 
   searchParams = {
-    key: '30410400-df54a4fa47e0d802e49478434',
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: 'true',
+    api_key: '9673c8c8f98cb6e489d5cad6b3789836',
+    language: 'en-US',
+    include_adult: 'false', // safesearch: 'true',
     page: 1,
-    per_page: 40,
   };
 
-  async fetchImages() {
-    const { searchParams, BASE_URL, RESPONSE_OK } = this;
-    searchParams.q = this.query;
-    searchParams.page = this.page;
-    const response = await axios.get(BASE_URL, { params: searchParams });
+  //https://api.themoviedb.org/3/trending/movie/day?api_key=9673c8c8f98cb6e489d5cad6b3789836&page=2
+
+  async getFilmsPopular(page = 1) {
+    const { searchParams, POPULAR_URL, RESPONSE_OK } = this;
+    searchParams.page = page;
+    const response = await axios.get(POPULAR_URL, {
+      params: searchParams,
+    });
     if (response.status !== RESPONSE_OK) {
       throw new Error(response.status);
     }
-    this.total = response.data.total;
-    this.fetchedImages += response.data.hits.length;
+    this.total = response.data.total_pages;
     return response.data;
   }
 
-// incrementPage() {
-//   this.page += 1;
-// }
+  //api.themoviedb.org/3/movie/913290?api_key=9673c8c8f98cb6e489d5cad6b3789836
 
-// resetPage() {
-//   this.page = 1;
-//   this.fetchedImages = 0;
-//   this.total = 0;
-// }
+  async getFilmsById(id) {
+    const { searchParams, MOVIE_ID_URL, RESPONSE_OK } = this;
+    const response = await axios.get(MOVIE_ID_URL + id, {
+      params: searchParams,
+    });
+    if (response.status !== RESPONSE_OK) {
+      throw new Error(response.status);
+    }
+    return response.data;
+  }
 
-// get query() {
-//   return this.searchQuery;
-// }
+  //https://api.themoviedb.org/3/search/movie?api_key=9673c8c8f98cb6e489d5cad6b3789836&language=en-US&query=www&page=1&include_adult=false
 
-// set query(newQuery) {
-//   this.searchQuery = newQuery;
-// }
-// }
+  async getFilmsSearched(query = 'www', page = 1) {
+    const { searchParams, SEARCH_URL, RESPONSE_OK } = this;
+    searchParams.page = page;
+    searchParams.query = query;
+    const response = await axios.get(SEARCH_URL, {
+      params: searchParams,
+    });
+    if (response.status !== RESPONSE_OK) {
+      throw new Error(response.status);
+    }
+    return response.data;
+  }
+
+  //https://api.themoviedb.org/3/movie/913290/videos?api_key=9673c8c8f98cb6e489d5cad6b3789836&language=en-US
+
+  //
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+    this.fetchedImages = 0;
+    this.total = 0;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
