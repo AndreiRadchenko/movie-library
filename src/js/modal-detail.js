c45d66cf4c6e9b1f69f540cca4d512dc5a2d946c
Oleksii Biletskyi <alenikbel@ukr.net>
2022-10-29 16:32:16 +0300

    add two functions for spinner

--- src/js/modal-detail.js
+++ src/js/modal-detail.js
@@ -1,13 +1,17 @@
 import FetchMoviService from '../js/moviedb/moviedb';
-
+import { spinnerPlay, spinnerStop } from './modal-spinner';
 const modalMovieService = new FetchMoviService();
 
 export function renderModalDetail({ target }) {
+  spinnerPlay();
   const id = target.getAttribute('data-id');
   modalMovieService
     .getFilmsById(id)
     .then(resolve => {
       console.log(resolve);
     })
-    .catch(error => console.log(error));
+    .catch(error => console.log(error))
+    .finally(() => {
+      spinnerStop();
+    });
 }
