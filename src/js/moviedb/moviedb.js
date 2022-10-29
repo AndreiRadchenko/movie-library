8180467e663e80a65bfea71f6639f95af89c44c5
AndreiRadchenko <d4100kx@gmail.com>
2022-10-30 01:11:19 +0300

    recouncil conflicts

--- src/js/moviedb/moviedb.js
+++ src/js/moviedb/moviedb.js
@@ -2,33 +2,19 @@
 // Ось мій ключ: api_key=9673c8c8f98cb6e489d5cad6b3789836
 //
 // Потрібно реалізувати наступні методи:
-<<<<<<< HEAD
-// 1. getFilmsPopular(page=1) --> повертає масив фільмів. Приймає номер сторінки, за замовчуванням 1
-// 2. getFilmsById(id)
-=======
 // 1.+ getFilmsPopular(page=1) --> повертає масив фільмів. Приймає номер сторінки, за замовчуванням 1
 // 2.+ getFilmsById(id)
->>>>>>> moviedb
 
 // 3. searchForMovie(movieName, page=1) --> повертає масив фільмів за рядком пошуку
 // 4. getMovieTrailer(moiveId)
 //
 // Клас має містити властивості totalResults, currentPage які будуть оновлюватися при кожному запиті
 //
-<<<<<<< HEAD
-//https://api.themoviedb.org/3/trending/movie/day?api_key=9673c8c8f98cb6e489d5cad6b3789836&page=2
-=======
-
->>>>>>> moviedb
 //api.themoviedb.org/3/movie/913290?api_key=9673c8c8f98cb6e489d5cad6b3789836
 
 import axios from 'axios';
 
-<<<<<<< HEAD
-export default class FetchMoviService {
-=======
 class FetchMoviService {
->>>>>>> moviedb
   constructor() {
     // this.searchQuery = '';
     this.page = 1;
@@ -45,14 +31,6 @@ class FetchMoviService {
 
   searchParams = {
     api_key: '9673c8c8f98cb6e489d5cad6b3789836',
-<<<<<<< HEAD
-    // image_type: 'photo',
-    // orientation: 'horizontal',
-    // safesearch: 'true',
-    page: 1,
-  };
-
-=======
     language: 'en-US',
     include_adult: 'false', // safesearch: 'true',
     page: 1,
@@ -60,35 +38,16 @@ class FetchMoviService {
 
   //https://api.themoviedb.org/3/trending/movie/day?api_key=9673c8c8f98cb6e489d5cad6b3789836&page=2
 
->>>>>>> moviedb
   async getFilmsPopular(page = 1) {
     const { searchParams, POPULAR_URL, RESPONSE_OK } = this;
     searchParams.page = page;
     const response = await axios.get(POPULAR_URL, {
       params: searchParams,
     });
-<<<<<<< HEAD
     if (response.status !== RESPONSE_OK) {
       throw new Error(response.status);
     }
     this.total = response.data.total_pages;
-    return response.data;
-  }
-
-  async getFilmsById(id) {
-    const { searchParams, MOVIE_ID_URL, RESPONSE_OK } = this;
-    const response = await axios.get(MOVIE_ID_URL + id, {
-      params: searchParams,
-    });
-    if (response.status !== RESPONSE_OK) {
-      throw new Error(response.status);
-    }
-=======
-    if (response.status !== RESPONSE_OK) {
-      throw new Error(response.status);
-    }
-    this.total = response.data.total_pages;
->>>>>>> moviedb
     return response.data;
   }
 
