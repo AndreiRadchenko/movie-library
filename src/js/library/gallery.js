1ae387038ca37f97bd1d0e91cd280cd965b04e9d
Vitaliy <103895059+vitaliy197811@users.noreply.github.com>
2022-11-02 22:08:32 +0200

    footer down + gallery center

--- src/js/library/gallery.js
+++ src/js/library/gallery.js
@@ -1,8 +1,10 @@
 import { renderModalDetail } from '../modal-detail-lib';
 import refs from '../refs';
-import galleryArray from '../library/array.json';
 import cloudStorage from '../firebase/cloudstorage';
 import { spinnerPlay, spinnerStop } from '../modal-spinner';
+import getGanreName from '../moviedb/genres';
+import poster from '../../images/gallery/file_not_found.jpg';
+
 const { WATCHED, QUEUE, NOT_ADDED } = cloudStorage.tags;
 
 export const renderGalleryLib = galleryArray => {
@@ -16,21 +18,33 @@ export const renderGalleryLib = galleryArray => {
         release_date,
         id,
         vote_average,
-      }) => `<div class="film__card" data-id=${id}>
-            <a class="film__link link" href="" onclick="event.preventDefault()">
-            <img class="film__img" src="https://image.tmdb.org/t/p/w500/${poster_path}" alt="" loading="lazy" />
-            <div class="film__info">
-                <h3 class="film__title">
-                ${original_title}
-                </h3>
-                <div class="film__details">
-            <div class="film__genre">${genre_ids}</div>
-            <div class="film__year">${release_date.substr(0, 4)}</div>
-            <div class="film__rating">${vote_average.toFixed(1)}</div>
-            </div>
-            </div>
-            </a>
-        </div>`
+      }) => {
+        let genres = genre_ids.map(getGanreName);
+        if (genres.length > 2) {
+          genres = `${genres[0]}, ${genres[1]}, Other`;
+        }
+        let posterUrl;
+        if (!poster_path) {
+          posterUrl = poster;
+        } else {
+          posterUrl = `https://image.tmdb.org/t/p/w500/${poster_path}`;
+        }
+        return `<div class="film__card" data-id=${id}>
+        <a class="film__link link" href="" onclick="event.preventDefault()">
+          <img class="film__img" src=${posterUrl} alt="" data-id=${id} loading="lazy"/>
+          <div class="film__info">
+            <h3 class="film__title">
+              ${original_title}
+            </h3>
+            <div class="film__details">
+          <div class="film__genre">${genres}</div>
+          <div class="film__year">${release_date.substr(0, 4)}</div>
+          <div class="film__rating">${vote_average.toFixed(1)}</div>
+          </div>
+          </div>
+        </a>
+      </div>`;
+      }
     )
     .join('');
   refs.filmGalleryLib.innerHTML = result;
@@ -40,7 +54,6 @@ cloudStorage
   .getUserCollections()
   .then(films => {
     const watchedFilms = films?.filter(film => film.tag === WATCHED);
-    // console.log(watchedFilms);
 
     refs.wachedBtn.classList.add('library__btn--currenly');
     refs.queueBtn.classList.remove('library__btn--currenly');
@@ -67,38 +80,6 @@ cloudStorage
   })
   .finally(() => spinnerStop());
 
-// getArreyWatched = () => {
-//   // const arreyWatched = localStorage.getItem('wached'); //тут повинен приходити массив фільмів з localStorage ті що в переглянуті
-//   const arreyWatched = galleryArray.results;
-//   refs.wachedBtn.classList.toggle('library__btn--currenly');
-//   refs.queueBtn.classList.remove('library__btn--currenly');
-//   if (arreyWatched) {
-//     // const arrWatched = JSON.parse(arreyWatched);
-//     renderGalleryLib(arreyWatched);
-//   } else {
-//     refs.filmGalleryLib.innerHTML = ' ';
-//   }
-// };
-
-// getArreyQueue = () => {
-//   // const arreyQueue = localStorage.getItem('queue'); //тут повинен приходити массив фільмів з localStorage ті що в черзі
-//   const arreyQueue = galleryArray.results;
-//   refs.queueBtn.classList.toggle('library__btn--currenly');
-//   refs.wachedBtn.classList.remove('library__btn--currenly');
-//   if (arreyQueue) {
-//     // const arrQueue = JSON.parse(arreyQueue);
-//     renderGalleryLib(arreyQueue);
-//   } else {
-//     refs.filmGalleryLib.innerHTML = ' ';
-//   }
-// };
-
-// changeTheme = () => {
-//   refs.body.classList.toggle('dark-theme');
-//   refs.filmGalleryLib.classList.toggle('dark-theme');
-// };
-
 refs.filmGalleryLib.addEventListener('click', renderModalDetail);
 
 export default renderGalleryLib;
-// refs.themeBtn.addEventListener('input', changeTheme);
