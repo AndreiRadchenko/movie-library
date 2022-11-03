742718c8ca9cf40d8ef32c06434f168f5c4d9b00
Tetiana-Shev <azaliya939@gmail.com>
2022-11-04 01:06:11 +0200

    Change Theme Modal-Login

--- src/js/index/header.js
+++ src/js/index/header.js
@@ -61,3 +61,44 @@ export function handleSubmit(event) {
 //   spinnerStop();
 //   return;
 // }
+
+
+// ========================= Change Theme================
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const bodyRef = document.querySelector('body');
+const switchRef = document.querySelector('#theme-switch-toggle');
+
+// дефолтная тема
+function defaultTheme() {
+  //const defaultTheme = localStorage.getItem('Theme');
+  const defaultTheme = localStorage['Theme'];
+
+  if (defaultTheme === Theme.DARK) {
+    bodyRef.classList.add(Theme.DARK);
+    switchRef.checked = true;
+  }
+}
+defaultTheme();
+
+//смена темы со светлой на тёмную
+
+bodyRef.addEventListener('change', onThemeChenge);
+
+function onThemeChenge(event) {
+  if (event.target.checked) {
+    bodyRef.classList.remove(Theme.LIGHT);
+    bodyRef.classList.add(Theme.DARK);
+
+    localStorage.setItem('Theme', Theme.DARK);
+  } else {
+    bodyRef.classList.remove(Theme.DARK);
+    bodyRef.classList.add(Theme.LIGHT);
+
+    localStorage.setItem('Theme', Theme.LIGHT);
+  }
+}
--- src/js/library/header.js
+++ src/js/library/header.js
@@ -15,3 +15,42 @@ function onQueueBtnClick() {
   refs.queueBtn.classList.add('library__btn--currenly');
   refreshLibrary();
 }
+
+
+// ========================= Change Theme================
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const bodyRef = document.querySelector('body');
+const switchRef = document.querySelector('#theme-switch-toggle');
+
+// дефолтная тема
+function defaultTheme() {
+  const defaultTheme = localStorage['Theme'];
+
+  if (defaultTheme === Theme.DARK) {
+    bodyRef.classList.add(Theme.DARK);
+    switchRef.checked = true;
+  }
+}
+defaultTheme();
+
+//смена темы со светлой на тёмную
+
+bodyRef.addEventListener('change', onThemeChenge);
+
+function onThemeChenge(event) {
+  if (event.target.checked) {
+    bodyRef.classList.remove(Theme.LIGHT);
+    bodyRef.classList.add(Theme.DARK);
+
+    localStorage.setItem('Theme', Theme.DARK);
+  } else {
+    bodyRef.classList.remove(Theme.DARK);
+    bodyRef.classList.add(Theme.LIGHT);
+
+    localStorage.setItem('Theme', Theme.LIGHT);
+  }
+}
