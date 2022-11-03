0b27f82d7c1cea1add1b1683438a59349b776653
Tetiana-Shev <azaliya939@gmail.com>
2022-11-03 20:36:30 +0200

    Add change theme

--- src/js/index/header.js
+++ src/js/index/header.js
@@ -1,6 +1,7 @@
 import movieService from '../moviedb/moviedb';
 import { renderGallery } from '../index/gallery';
 import { spinnerPlay, spinnerStop } from '../modal-spinner';
+import { doc } from 'firebase/firestore';
 
 // ця функція повинна визиватись по кліку на лупу і підставляти замість
 // 'search query' рядок з інпута
@@ -37,5 +38,46 @@ export function handleSubmit(event) {
         renderGallery(resolve.results);
       })
       .finally(() => spinnerStop());
+  }
+}
+
+
+// ========================= Change Theme================
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const bodyRef = document.querySelector('body');
+const switchRef = document.querySelector('#theme-switch-toggle');
+
+// дефолтная тема
+function defaultTheme() {
+  //const defaultTheme = localStorage.getItem('Theme');
+  const defaultTheme = localStorage['Theme'];
+
+  if (defaultTheme === Theme.DARK) {
+    bodyRef.classList.add(Theme.DARK);
+    switchRef.checked = true;
+  }
+}
+defaultTheme();
+
+//смена темы со светлой на тёмную
+
+bodyRef.addEventListener('change', onThemeChenge);
+
+function onThemeChenge(event) {
+  if (event.target.checked) {
+    bodyRef.classList.remove(Theme.LIGHT);
+    bodyRef.classList.add(Theme.DARK);
+
+    localStorage.setItem('Theme', Theme.DARK);
+  } else {
+    bodyRef.classList.remove(Theme.DARK);
+    bodyRef.classList.add(Theme.LIGHT);
+
+    localStorage.setItem('Theme', Theme.LIGHT);
   }
 }
