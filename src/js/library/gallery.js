3de39db707293824ea1d8520aacb6e6c18e6fe33
Vitaliy <103895059+vitaliy197811@users.noreply.github.com>
2022-11-03 16:51:26 +0200

    gallery library + hover gallery + header

--- src/js/library/gallery.js
+++ src/js/library/gallery.js
@@ -1,6 +1,8 @@
 import { renderModalDetail } from '../modal-detail-lib';
 import refs from '../refs';
 import { refreshLibrary } from '../modal-detail-lib';
+import getGanreName from '../moviedb/genres';
+import poster from '../../images/gallery/file_not_found.jpg';
 
 export const renderGalleryLib = galleryArray => {
   refs.filmGalleryLib.innerHTML = ' ';
@@ -13,21 +15,33 @@ export const renderGalleryLib = galleryArray => {
         release_date,
         id,
         vote_average,
-      }) => `<div class="film__card" data-id=${id}>
-            <a class="film__link link" href="" onclick="event.preventDefault()">
-            <img class="film__img" src="https://image.tmdb.org/t/p/w500/${poster_path}" alt="" loading="lazy" />
-            <div class="film__info">
-                <h3 class="film__title">
-                ${original_title}
-                </h3>
-                <div class="film__details">
-            <div class="film__genre">${genre_ids}</div>
-            <div class="film__year">${release_date.substr(0, 4)}</div>
-            <div class="film__rating">${vote_average.toFixed(1)}</div>
-            </div>
-            </div>
-            </a>
-        </div>`
+      }) => {
+        let genres = genre_ids.map(getGanreName);
+        if (genres.length > 2) {
+          genres = `${genres[0]}, ${genres[1]}, Other`;
+        }
+        let posterUrl;
+        if (!poster_path) {
+          posterUrl = poster;
+        } else {
+          posterUrl = `https://image.tmdb.org/t/p/w500/${poster_path}`;
+        }
+        return `<div class="film__card" data-id=${id}>
+        <a class="film__link link" href="" onclick="event.preventDefault()">
+          <img class="film__img" src=${posterUrl} alt="" data-id=${id} loading="lazy"/>
+          <div class="film__info">
+            <h3 class="film__title">
+              ${original_title}
+            </h3>
+            <div class="film__details">
+          <div class="film__genre">${genres}</div>
+          <div class="film__year">${release_date.substr(0, 4)}</div>
+          <div class="film__rating">${vote_average.toFixed(1)}</div>
+          </div>
+          </div>
+        </a>
+      </div>`;
+      }
     )
     .join('');
   refs.filmGalleryLib.innerHTML = result;
